126f3488004470882ada3b05c1d4ec2ca914feaa
ddl <happyincent@gmail.com>
2019-05-16 15:31:40 +0800

    Add live555 rtsp-proxy

--- star-edge/node/generate.sh
+++ star-edge/node/generate.sh
@@ -1,24 +1,36 @@
-NODE_NAME=TX2
-DEVICES=(\
-    TX2_0 \
-)
-TOKENS=(\
-    8a88de1a-76ab-4d38-ba44-de4303918f4f \
-)
-
-# NODE_NAME=VM0
+# NODE_NAME=TX2
 # DEVICES=(\
-#     VM0_0 \
-#     VM0_1 \
-#     VM0_2 \
-#     VM0_3 \
+#     TX2_0 \
 # )
 # TOKENS=(\
-#     d1b67884-54e6-4d90-9b82-7eb62b440d98 \
-#     96f6c0a6-a349-40bf-8388-6f6be6354810 \
-#     ae6d3949-bb96-478d-afda-4d08d80a2d9a \
-#     c2d153f2-180c-40b6-a7e1-f95d4b81f23c \
+#     8a88de1a-76ab-4d38-ba44-de4303918f4f \
 # )
+
+NODE_NAME=VM0
+DEVICES=(\
+    VM0_0 \
+    VM0_1 \
+    VM0_2 \
+    VM0_3 \
+    VM0_4 \
+    VM0_5 \
+    VM0_6 \
+    VM0_7 \
+    VM0_8 \
+    VM0_9 \
+)
+TOKENS=(\
+    d1b67884-54e6-4d90-9b82-7eb62b440d98 \
+    96f6c0a6-a349-40bf-8388-6f6be6354810 \
+    ae6d3949-bb96-478d-afda-4d08d80a2d9a \
+    c2d153f2-180c-40b6-a7e1-f95d4b81f23c \
+    91584ea8-ae3d-4bf2-bd3c-a6192887801b \
+    2fe2d423-2445-4897-a196-8cfd27e6d132 \
+    39159d54-8fbf-4002-b3c9-bfabf4231ed9 \
+    a91efb2c-38d0-4693-93f8-77887a0771c6 \
+    fea16d76-3500-4d29-84d5-67201c154bf8 \
+    6fd52eb2-df01-4772-b855-80283797e690 \
+)
 
 # NODE_NAME=RPi
 # DEVICES=(\
@@ -46,5 +58,9 @@ for i in "${!DEVICES[@]}"; do
             s|\${DEVICE}|${DEVICES[$i]}|g; \
             s|\${TOKEN}|${TOKENS[$i]}|g" \
             _$f > ${NODE_NAME}_${DEVICES[$i]}-$f
+    
+        if [[ ${DEVICES[$i]} = VM* ]] && [[ $f = ffmpeg.yml ]] ; then
+            sed -i "s|\${RTSP_URI}|\${RTSP_PROXY_URI}|;" ${NODE_NAME}_${DEVICES[$i]}-$f
+        fi
     done
 done
