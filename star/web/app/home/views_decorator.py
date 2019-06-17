efdbff714854db9b03792223871f12c8a31b3ed6
ddl <happyincent@gmail.com>
2019-06-17 21:37:18 +0800

    Define decorator to return 403 if not staff

--- star/web/app/home/views_decorator.py
+++ star/web/app/home/views_decorator.py
@@ -1,10 +1,26 @@
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.contrib.admin.views.decorators import staff_member_required
+# from django.contrib.admin.views.decorators import staff_member_required
 
 from allauth.account.decorators import verified_email_required
 
 from django.views.decorators.csrf import csrf_protect
 
+from functools import wraps
+from django.http import HttpResponseForbidden
+from django.core.exceptions import PermissionDenied
+
+def user_passes_test_403(test_func):
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            if test_func(request.user):
+                return view_func(request, *args, **kwargs)
+            raise PermissionDenied
+        return _wrapped_view
+    return decorator
+
+staff_member_required_403 = user_passes_test_403(lambda u: u.is_active and u.is_staff)
+
 legal_user = [login_required, verified_email_required]
-legal_staff_user = [login_required, verified_email_required, staff_member_required]
+legal_staff_user = [login_required, verified_email_required, staff_member_required_403]
