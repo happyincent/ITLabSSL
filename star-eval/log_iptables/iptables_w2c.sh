15bca6557bf174bd931c64a75e0aef55fd86965b
ddl <happyincent@gmail.com>
2019-07-25 14:48:38 +0800

    Add scripts

---
+++ star-eval/log_iptables/iptables_w2c.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+insert() {
+    iptables -t mangle -I PREROUTING -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/mangle-PREROUTE ' --log-level debug
+    iptables -t nat -I PREROUTING -p tcp  --dport 22 -j LOG --log-prefix 'wtc/iptable/nat-PREROUTE ' --log-level debug
+
+    iptables -t filter -I FORWARD -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/filter-forward ' --log-level debug
+    iptables -t mangle -I FORWARD -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/mangle-FORWARD ' --log-level debug
+
+    iptables -t mangle -I POSTROUTING -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/mangle-POSTROUTE ' --log-level debug
+    iptables -t nat -I POSTROUTING -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/nat-POSTROUTE ' --log-level debug
+
+    iptables -t mangle -I PREROUTING -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/mangle-PREROUTE ' --log-level debug
+    iptables -t nat -I PREROUTING -p tcp  --dport 62422 -j LOG --log-prefix 'wtc/iptable/nat-PREROUTE ' --log-level debug
+
+    iptables -t filter -I FORWARD -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/filter-forward ' --log-level debug
+    iptables -t mangle -I FORWARD -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/mangle-FORWARD ' --log-level debug
+
+    iptables -t mangle -I POSTROUTING -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/mangle-POSTROUTE ' --log-level debug
+    iptables -t nat -I POSTROUTING -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/nat-POSTROUTE ' --log-level debug
+ }
+
+delete() {
+    iptables -t mangle -D PREROUTING -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/mangle-PREROUTE ' --log-level debug
+    iptables -t nat -D PREROUTING -p tcp  --dport 22 -j LOG --log-prefix 'wtc/iptable/nat-PREROUTE ' --log-level debug
+
+    iptables -t filter -D FORWARD -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/filter-forward ' --log-level debug
+    iptables -t mangle -D FORWARD -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/mangle-FORWARD ' --log-level debug
+
+    iptables -t mangle -D POSTROUTING -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/mangle-POSTROUTE ' --log-level debug
+    iptables -t nat -D POSTROUTING -p tcp --dport 22 -j LOG --log-prefix 'wtc/iptable/nat-POSTROUTE ' --log-level debug
+
+    iptables -t mangle -D PREROUTING -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/mangle-PREROUTE ' --log-level debug
+    iptables -t nat -D PREROUTING -p tcp  --dport 62422 -j LOG --log-prefix 'wtc/iptable/nat-PREROUTE ' --log-level debug
+
+    iptables -t filter -D FORWARD -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/filter-forward ' --log-level debug
+    iptables -t mangle -D FORWARD -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/mangle-FORWARD ' --log-level debug
+
+    iptables -t mangle -D POSTROUTING -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/mangle-POSTROUTE ' --log-level debug
+    iptables -t nat -D POSTROUTING -p tcp --dport 62422 -j LOG --log-prefix 'wtc/iptable/nat-POSTROUTE ' --log-level debug
+}
+
+check() {
+    count=`iptables-save | grep wtc | wc -l`
+    if [ "$count" == "0" ]; then
+        echo "Delete Success"
+    else
+        echo "Delete Fail, Use the iptables-save to check what rules still exist"
+    fi
+}
+
+
+if [ "$1" == "d" ]; then
+delete
+check
+else
+insert
+fi
